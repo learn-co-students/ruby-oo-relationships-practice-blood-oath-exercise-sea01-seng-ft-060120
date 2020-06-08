3cc0e42639c8dd8fb3f7e47c3cc1639a0ef3d2a5
zanhe <zhertica@gmail.com>
2020-06-08 14:33:22 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
 require 'bundler/setup'
 Bundler.require
-require_rel '../app'
+require_rel '../app' 
+
+class Cult
+    attr_accessor :name, :location, :founding_year
+
+    def initialize(name)
+        @name = name
+        @location = location
+        @founding_year = int
+    end
+end
+
